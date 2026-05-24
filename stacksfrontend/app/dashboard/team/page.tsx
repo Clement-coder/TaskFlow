71bb572e5db3618f02f2c8c502ed4ebfda40c5f8
TaskFlow Dev <dev@taskflow.xyz>
2026-05-24 22:08:43 +0000

    style(team): increase team page section spacing to space-y-7

--- stacksfrontend/app/dashboard/team/page.tsx
+++ stacksfrontend/app/dashboard/team/page.tsx
@@ -66,7 +66,7 @@ export default function TeamPage() {
   };
 
   return (
-    <div className="space-y-6 pb-4">
+    <div className="space-y-7 pb-4">
       {/* Toast */}
       <AnimatePresence>
         {toast && (
